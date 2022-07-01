e6c91d67a2a0921432254a2c668f6d501caf884c
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-07-01 19:20:16 +0300

    Import Home in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import './App.css'
-import Header from './components/Header'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
-      <Header />
+      <Home />
     </div>
   )
 }
