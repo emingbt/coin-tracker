44b616c7bcf2d6c6d0b7b7b76840fcf34d5d28f4
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-07-02 17:50:52 +0300

    Import useParams, get page value and give it to Coins List as a prop

--- src/pages/AllCoins.tsx
+++ src/pages/AllCoins.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import Header from "../components/Header"
 import CoinsList from "../components/CoinsList"
 import Footer from "../components/Footer"
+import { useParams } from "react-router-dom"
 
 const Wrapper = styled.div`
   display: flex;
@@ -11,11 +12,17 @@ const Wrapper = styled.div`
   justify-content: center;
 `
 
+type Params = {
+  page: string
+}
+
 const AllCoins = () => {
+  let { page } = useParams<Params>()
+
   return (
     <Wrapper>
       <Header />
-      <CoinsList />
+      <CoinsList page={page || "1"} />
       <Footer />
     </Wrapper>
   )
