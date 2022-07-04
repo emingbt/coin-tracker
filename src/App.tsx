d6f5b05c8bc0764231e96a331278066ec1684069
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-07-04 18:14:39 +0300

    Import Coin page and create a route for it

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import AllCoins from './pages/AllCoins'
 import { Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import Search from './pages/Search'
+import Coin from './pages/Coin'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path='/' element={<Home />} />
       <Route path='/allcoins/:page' element={<AllCoins />} />
       <Route path='/search/:query' element={<Search />} />
+      <Route path='/coin/:coinId' element={<Coin />} />
     </Routes>
   )
 }
