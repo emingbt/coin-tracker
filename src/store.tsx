51e69026514052eae2f1db8a263ccd81e37fe81f
Emin Göbütoğlu <mehmet.e.gbt@gmail.com>
2022-07-07 13:42:04 +0300

    Update store file

--- src/store.tsx
+++ src/store.tsx
@@ -1,17 +1,25 @@
 import create from "zustand"
+import { persist } from "zustand/middleware"
 
-type State = {
+interface CoinState {
   favoriteCoins: Array<string>
-  addFavorite: (coin:string) => void
-  removeFavorite: (newFavortieCoins:Array<string>) => void
+  addFavorite: (coin: string) => void
+  removeFavorite: (newFavortieCoins: Array<string>) => void
 }
 
-const useStore = create<State>((set) => ({
-  favoriteCoins: [],
-  addFavorite: (coin:string) =>
-    set((state) => ({favoriteCoins: [...state.favoriteCoins, coin] })),
-  removeFavorite: (newFavoriteCoins:Array<string>) => 
-    set((state) => ({favoriteCoins: [...newFavoriteCoins]}))
-}))
+
+const useStore = create(
+  persist<CoinState>((set) => ({
+    favoriteCoins: [],
+    addFavorite: (coin: string) =>
+      set((state) => ({ favoriteCoins: [...state.favoriteCoins, coin] })),
+    removeFavorite: (newFavoriteCoins: Array<string>) =>
+      set((state) => ({ favoriteCoins: [...newFavoriteCoins] }))
+  }),
+    {
+      name: 'favorite-coin-storage'
+    }
+  )
+)
 
 export default useStore
